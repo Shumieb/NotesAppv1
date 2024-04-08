ae30fc77ae8551efd9afbf73b51cf1c26b137ced
Shumieb <shumiebc@gmail.com>
2024-04-08 14:29:48 +0100

    notes-list component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
 import { AddNoteComponent } from '../add-note/add-note.component';
+import { NotesListComponent } from '../notes-list/notes-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, AddNoteComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, AddNoteComponent, NotesListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
