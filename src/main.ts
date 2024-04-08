3b16c7b85773e7dad0c773fac0a5cd2edc9b4bee
Shumieb <shumiebc@gmail.com>
2024-04-08 15:34:51 +0100

    Color changed on all components

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { Routes, provideRouter } from '@angular/router';
+
+const routes: Routes = [
+  { path: "", component: AppComponent },
+]
+
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
